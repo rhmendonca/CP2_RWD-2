8ea2435ae432a37ba26c8b1602e88aecf3ae3513
Neveszera <gabrielgng26@gmail.com>
2023-09-15 04:04:11 -0300

    Finalização Página Error 404.

--- src/routes/Error/Error.jsx
+++ src/routes/Error/Error.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Error.module.css';
 
-function NotFound() {
+function Error() {
   return (
-    <div>
-      <h2>Erro 404</h2>
-      <p>Página não encontrada.</p>
+    <div className={styles.container}>
+      <h2 className={styles.heading}>Erro 404 - Página não encontrada</h2>
+      <p className={styles.message}>
+        Parece que você se perdeu no universo digital. Que tal voltar para a
+        segurança da <Link to="/" className={styles.link}>página inicial</Link>?
+      </p>
+      <div className={styles.gifContainer}>
+        <img
+          src="https://media.giphy.com/media/QAxqYgH6b0vvD0Jm6e/giphy.gif" // Substitua com o link do seu GIF animado
+          alt="Erro 404"
+          className={styles.gif}
+        />
+      </div>
     </div>
   );
 }
 
-export default NotFound;
+export default Error;
