160e44a6ad305c56ff6088b45c786e7d286195f2
Neveszera <gabrielgng26@gmail.com>
2023-09-15 02:16:58 -0300

    Reorganização das pastas e estilização header.

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './CSS/Header.module.css';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>NevesStore</h1>
+      <nav>
+        <Link to="/" className={styles.headerLink}>
+          Home
+        </Link>
+        <Link to="/aparelhos" className={styles.headerLink}>
+          Aparelhos
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
