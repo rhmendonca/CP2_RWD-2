7c59195f5b9c25005ad2a4e3a10761809268e69e
Neveszera <gabrielgng26@gmail.com>
2023-09-15 02:22:47 -0300

    Página Home criada e estilizada.

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
+import aparelhosData from '../../data/aparelhosData.jsx';
+
 
 function Home() {
+  
+  const destaques = aparelhosData.slice(0, 2);
+
   return (
-    <div>
-      <h2>Promoções</h2>
-      <p>Propaganda 1</p>
-      <p>Propaganda 2</p>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>NevesStore - Seu Destino para Smartphones e Tablets</h1>
+      <div className={styles.highlightsContainer}>
+        {destaques.map((destaqueId) => {
+          const destaque = aparelhosData.find((item) => item.id === destaqueId);
+
+          return (
+            <div key={destaque.id} className={styles.highlight}>
+              <img
+                src={destaque.imagem}
+                alt={destaque.nome}
+                className={styles.highlightImage}
+              />
+              <h3>{destaque.nome}</h3>
+              <p>{destaque.descricaoCurta}</p>
+              <p className={styles.smartphonePrice}>{destaque.preco}</p>
+              <Link to={`/aparelhos/${destaque.id}`} className={styles.viewButton}>
+                Ver Detalhes
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
