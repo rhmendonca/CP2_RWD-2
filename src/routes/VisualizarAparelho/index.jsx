d72476983aaacf7c13083d56a09e761c200a5b18
rhmendonca <rafaelhenriquedemendonca@outlook.com>
2023-09-21 20:36:41 -0300

    Funcionalidade deletar desenvolvida.

--- src/routes/VisualizarAparelho/index.jsx
+++ src/routes/VisualizarAparelho/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import aparelhosData from '../../data/aparelhosData.jsx';
 import styles from './index.module.css';
 
 function VisualizarAparelho() {
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  // Encontrar o aparelho pelo ID 
   const aparelho = aparelhosData.find((item) => item.id === parseInt(id));
+
+//Função para lidar com a exclusão do aparelho
+const handleDelete = () => {
+  //Remove o aparelho da lista de dados (aparelhosData)
+  const index = aparelhosData.findIndex((item) => item.id === 
+  aparelho.id);
+  if (index !== -1){
+    aparelhosData.splice(index, 1);
+    //Navega de volta para a página de aparelhos
+    navigate('/aparelhos')
+  }
+
+};
 
   if (!aparelho) {
     return <p>Aparelho não encontrado.</p>;
@@ -21,7 +37,8 @@ function VisualizarAparelho() {
       />
       <p className={styles.descricaoExtensa}>{aparelho.descricaoExtensa}</p>
       <p className={styles.preco}>Preço: ${aparelho.preco}</p>
-      <a href="/aparelhos" className={styles.goBackLink}>Voltar</a>
+      <button onClick={handleDelete}>Excluir Aparelho</button>
+      <button onClick={() => navigate('/aparelhos')} className={styles.goBackLink}>Voltar para a lista de Aparelhos</button>
     </div>
   );
 }
